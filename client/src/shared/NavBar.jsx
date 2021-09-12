3365f3a6c32103786c3e438f89489450195ba15a
donjude-eng <donjudejoseph123@gmail.com>
2021-09-12 23:18:50 +0530

    Add update bookand client

--- client/src/shared/NavBar.jsx
+++ client/src/shared/NavBar.jsx
@@ -1,29 +1,21 @@
-import {
-  Flex,
-  Text,
-  Box,
-  Link as StyledLink,
-  Image,
-} from "rebass/styled-components";
-import { Link } from "react-router-dom";
-import { Container } from "./Container"
-import logo from "./logo.svg";
+import {Flex , Box, Link as StyledLink, Image} from "rebass/styled-components"
+import {Link} from "react-router-dom"
+
 
 export const NavBar = () => {
-  return (
-    <Flex bg="black" color="white" justifyContent="center">
-      <Container>
-        <Flex px={2} width="100%" alignItems="center">
-          <Image size={20} src={logo} />
-          <Link component={StyledLink} variant="nav" to="/">
-            React Query CRUD
-          </Link>
-          <Box mx="auto" />
-          <Link component={StyledLink} variant="nav" to="/create-book">
-            + Add new book
-          </Link>
+    return (
+        <Flex bg="black" color="white" justifyContent="center">
+            <Flex px={2} width="100%" alignItems="center">
+                <Link component={StyledLink} variant="nav" to="/">
+                    React Query CRUD
+                </Link>
+                <Box mx="auto">
+                <Link component={StyledLink} variant="nav" to="/create-book">
+                    React Query CRUD
+                    </Link>
+
+            </Flex>
         </Flex>
-      </Container>
-    </Flex>
-  );
-};
+
+    )
+}
